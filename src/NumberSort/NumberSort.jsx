14342a13cef9fc421b9d0723cb6d1ce60d93769b
wanxiang2 <wanxiang@buffalo.edu>
2024-06-21 13:07:39 -0400

    Saves user input from the boxes into an Array.

--- src/NumberSort/NumberSort.jsx
+++ src/NumberSort/NumberSort.jsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from "react";
 import './NumberSort.css';
 
 function NumberSort() {
-    
+    // This is to create and render the initial boxes onto the screen.
     const [numBoxes, setNumBoxes] = useState([]);
 
     useEffect(() => {
@@ -14,15 +14,25 @@ function NumberSort() {
         setNumBoxes(boxes);
     }, []);
 
+    // This saves the numbers that the user inputs into the boxes into an array called inputValues.
+    const [inputValues, setInputValues] = useState(new Array(5));
+
+    const inputChangeHandler = (index, event) => {
+        const newInputValues = [...inputValues];
+        newInputValues[index] = event.target.value;
+        setInputValues(newInputValues);
+    };
+
     return (
         <div className="container">
             <div className="boxes">
                 {numBoxes.map((myBox, boxNumber) => (
-                    <input key={boxNumber} className="box" type="number"></input>
+                    <input key={boxNumber} className="box" type="number" onChange={(event) => inputChangeHandler(boxNumber, event)}></input>
                 ))}
             </div>
 
             <button className="button">Sort</button>
+
         </div>
         
     );
