a9b258c7fb3b7642782eef6e483ede990917be4b
wanxiang2 <wanxiang@buffalo.edu>
2024-06-26 20:55:36 -0400

    Now boxes with no user input are removed when the user clicks the button. And the inputValues state removes all empty user inputs from the array.

--- src/NumberSort/NumberSort.jsx
+++ src/NumberSort/NumberSort.jsx
@@ -17,11 +17,15 @@ function NumberSort() {
     }, []);
 
     // This saves the numbers that the user inputs into the boxes into an array called inputValues.
-    const [inputValues, setInputValues] = useState(new Array(5).fill(0));
+    const [inputValues, setInputValues] = useState(new Array(5).fill(null));
 
     const inputChangeHandler = (index, event) => {
         const newInputValues = [...inputValues];
-        newInputValues[index] = parseInt(event.target.value);
+        newInputValues[index] = event.target.value === "" ? null : parseInt(event.target.value);  // If the user types a number in a box
+                                                                                                  // and then deletes it then the value in 
+                                                                                                  // <input> will be "". This changes "" 
+                                                                                                  // back to null so we can remove it when 
+                                                                                                  // the button is clicked.
         setInputValues(newInputValues);
     };
 
@@ -34,6 +38,19 @@ function NumberSort() {
     const clickHandler = () => {
         //let sortedArray = mergeSort(inputValues);
         //setInputValues(sortedArray);
+
+        // Restructures the inputValues array so that input tags with no user input at the time
+        // of the button being clicked is removed from the inputValues array.
+        const prevInputValues = [...inputValues];
+        const filteredInputValues = prevInputValues.filter(num => (num !== null));
+        setInputValues(filteredInputValues);
+
+        // Restructures the boxes layout on the webpage when the button is clicked so that the
+        // number of boxes shown matches the number of values the inputValues array has.
+        const newNumBoxes = new Array(filteredInputValues.length).fill([]);
+        setNumBoxes(newNumBoxes);
+
+        // Animates the button fading away and the boxes moving up when the button is clicked.
         setColorChange(true);
         setButtonFade(true);
         setTimeout(() => {
@@ -45,12 +62,11 @@ function NumberSort() {
         <div className="container">
             <div className={`boxes ${moveUp ? 'move-up' : ''}`}>
                 {numBoxes.map((myBox, boxNumber) => (
-                    <input key={boxNumber} className={`box ${colorChange ? 'color-change' : ''}`} type="number" onChange={(event) => inputChangeHandler(boxNumber, event)}></input>
+                    <input key={boxNumber} className={`box ${colorChange ? 'color-change' : ''}`} type="number" value={inputValues[boxNumber] !== null ? inputValues[boxNumber] : ""} onChange={(event) => inputChangeHandler(boxNumber, event)}></input>
                 ))}
             </div>
 
             <button className={`button ${buttonFade ? 'button-fade' : ''}`} onClick={clickHandler}>Sort</button>
-           
         </div>
          
     );
